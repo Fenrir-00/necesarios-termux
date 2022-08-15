d97870029d47ee8a1c7c901e88d8abe1cf72be6c
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-15 23:24:24 +0200

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -1,5 +1,5 @@
 import os,sus
-import baner
+from banner import*
 class color:
     morado = '\033[95m'
     blanco = '\033[97m'
@@ -9,3 +9,4 @@ class color:
     amarillo = '\033[93m'
     rojo = '\033[91m'
     fin = '\033[0m'
+banner()
