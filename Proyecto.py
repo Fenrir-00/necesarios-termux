c4eb224ba8a2cb5ef7aadc43787ae8047c9f8b30
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-15 23:22:56 +0200

    Create Proyecto.py

---
+++ Proyecto.py
@@ -0,0 +1,11 @@
+import os,sus
+import baner
+class color:
+    morado = '\033[95m'
+    blanco = '\033[97m'
+    cyan = '\033[96m'
+    azul  = '\033[94m'
+    verde = '\033[92m'
+    amarillo = '\033[93m'
+    rojo = '\033[91m'
+    fin = '\033[0m'
